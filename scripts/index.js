8a02c65e580af19e64cbf7c2bd8f1324f2d28d03
NikitaBondarev1 <Bondarev363@yandex.ru>
2023-01-08 17:33:34 +0300

    fix-project

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,22 @@
 let profileName = document.querySelector(".profile__name");
 let profileSubtitle = document.querySelector(".profile__subtitle");
+let popupInfo = document.querySelector(".popup__info");
+let nameInput = document.querySelector(".popup__element_name_input");
+let subtitleWork = document.querySelector(".popup__subtitle");
 const buttonEdd = document.querySelector(".profile__edit");
 const popup = document.querySelector(".popup");
 const popupContainer = document.querySelector(".popup__container");
 const popupClose = document.querySelector(".popup__close");
-let popupInfo = document.querySelector(".popup__info");
-let nameInput = document.querySelector(".popup__name_input");
-let subtitleWork = document.querySelector(".popup__subtitle");
 
-buttonEdd.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  popup.classList.add("popup_active");
+buttonEdd.addEventListener("click", () => {
+  popup.classList.add("popup_opened");
 
   nameInput.value = profileName.textContent;
   subtitleWork.value = profileSubtitle.textContent;
 });
 
-popupClose.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  popup.classList.remove("popup_active");
+popupClose.addEventListener("click", () => {
+  popup.classList.remove("popup_opened");
 });
 
 function formSubmit(evt) {
@@ -26,6 +24,10 @@ function formSubmit(evt) {
 
   profileName.textContent = nameInput.value;
   profileSubtitle.textContent = subtitleWork.value;
-  popup.classList.remove("popup_active");
+  closeSubmit();
+}
+
+function closeSubmit() {
+  popup.classList.remove('popup_opened');
 }
 popupInfo.addEventListener("submit", formSubmit);
